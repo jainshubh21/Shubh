084e7ddb7ec400a32229ae733ff43ce2c2f77232
Shubham jain <jainshubh21@gmail.com>
2018-10-24 11:59:34 +0530

    Basic program

---
+++ Addition.java
@@ -0,0 +1,13 @@
+
+public class subtraction {
+    public static void main(String args[])
+    {
+    	int a,b,sub;
+    	a = Integer.parseInt(args[0]);
+    	b = Integer.parseInt(args[1]);
+    	
+    	sub = a-b;
+    	
+    	System.out.println("The Subtraction is : "+sub);
+    }
+}
