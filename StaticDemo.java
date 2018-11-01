22586e1d52fa614ab397052554ec8c832aee74e6
Shubham jain <jainshubh21@gmail.com>
2018-11-01 11:49:45 +0530

    Basic Program

--- StaticDemo.java
+++ StaticDemo.java
@@ -13,7 +13,7 @@ public class StaticDemo
     this.EmpName = EmpName;
     this.Salary = Salary;
     
-    TotalSalary = TotalSalary + Salary;
+    //TotalSalary = TotalSalary + Salary;
   }
   public void ShowDetails()
   {
@@ -22,17 +22,23 @@ public class StaticDemo
 	  System.out.println("Employee Salary:" + Salary);
   }
   
+  public static void DisplayTotalsalary()
+  {
+	  System.out.println("The total salary is:" + TotalSalary );
+
+  }
+  
   public static void main(String []args)
   {
 	  StaticDemo Staticobj = new StaticDemo("S1001" , "Shubham" , 50000);
 	  StaticDemo Staticobj1 = new StaticDemo("S1002" , "Irfan" , 45000);
 	  StaticDemo Staticobj2 = new StaticDemo("S1003" , "Shivam" , 40000);
 	  
-	  	  
+	  TotalSalary =Staticobj.Salary+Staticobj1.Salary+Staticobj2.Salary; 	  
 	  Staticobj.ShowDetails();
 	  Staticobj1.ShowDetails();
 	  Staticobj2.ShowDetails();
-	  System.out.println("The total salary is:" + Staticobj1.TotalSalary);
+	  StaticDemo.DisplayTotalsalary();
   }
 
 }
